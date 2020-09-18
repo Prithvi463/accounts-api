555816799bddd3dbcdb9c1626020f771a8bc4119
Prithvi463 <prudvi463@gmail.com>
2020-09-19 00:09:03 +0530

    Updated Vendor table

---
+++ AICloud.Accounts.Api/Migrations/202009181831080_vendorchange.cs
@@ -0,0 +1,16 @@
+﻿namespace AICloud.Accounts.Api.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class vendorchange : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- AICloud.Accounts.Api/Models/Vendor.cs
+++ AICloud.Accounts.Api/Models/Vendor.cs
@@ -8,16 +8,16 @@ namespace AICloud.Accounts.Api.Models
 	public class Vendor
 	{
 		public int Id { get; set; }
-		public string Name { get; set; }
+		public string VendorId { get; set; }
+		public string CompanyName { get; set; }
 		public string MobileNo { get; set; }
 		public string Email { get; set; }
 		public string WorkPhone { get; set; }
-		public string HouseNo { get; set; }
-		public string Street { get; set; }
+		public string Address1 { get; set; }
+		public string Address2 { get; set; }
 		public string City { get; set; }
 		public string State { get; set; }
-		public string County { get; set; }
 		public string Country { get; set; }
-		public string PinCode { get; set; }
+		public string ZipCode { get; set; }
 	}
 }
