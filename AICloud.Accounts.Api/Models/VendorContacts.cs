1a05818cb93f38cc9a100a3ab16c80fefc36e1f4
Prithvi463 <prudvi463@gmail.com>
2020-09-19 00:20:04 +0530

    Added VendorContacts table

---
+++ AICloud.Accounts.Api/Models/VendorContacts.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AICloud.Accounts.Api.Models
+{
+	public class VendorContacts
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Email { get; set; }
+		public string Phone { get; set; }
+		public string Address1 { get; set; }
+		public string Address2 { get; set; }
+		[ForeignKey("Vendor")] public int Vendor_Id { get; set; }
+		public Vendor Vendor { get; set; }
+
+	}
+}
