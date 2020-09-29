67eded768f5503711977dc2977cb2a4a30b42335
Prithvi463 <prithvirajiv@outlook.com>
2020-09-30 00:12:56 +0530

    Enabled Automatic Migration

--- AICloud.Accounts.Api/Migrations/Configuration.cs
+++ AICloud.Accounts.Api/Migrations/Configuration.cs
@@ -9,7 +9,7 @@
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
             ContextKey = "AICloud.Accounts.Api.Context.AccountsDbContext";
         }
 
