73906638b89a5b7cda97790f63ae7780b45bc321
Prithvi463 <prithvirajiv@outlook.com>
2020-09-30 01:04:57 +0530

    Migration Deleted

--- AICloud.Accounts.Api/Migrations/202009182007407_ARInvoice.cs
+++ AICloud.Accounts.Api/Migrations/202009182007407_ARInvoice.cs
@@ -77,30 +77,11 @@
                     })
                 .PrimaryKey(t => t.Id);
             
-            AddColumn("dbo.Vendors", "VendorId", c => c.String());
-            AddColumn("dbo.Vendors", "CompanyName", c => c.String());
-            AddColumn("dbo.Vendors", "Address1", c => c.String());
-            AddColumn("dbo.Vendors", "Address2", c => c.String());
-            AddColumn("dbo.Vendors", "ZipCode", c => c.String());
-            DropColumn("dbo.Vendors", "Name");
-            DropColumn("dbo.Vendors", "HouseNo");
-            DropColumn("dbo.Vendors", "Street");
-            DropColumn("dbo.Vendors", "County");
-            DropColumn("dbo.Vendors", "PinCode");
+          
         }
         
         public override void Down()
-        {
-            AddColumn("dbo.Vendors", "PinCode", c => c.String());
-            AddColumn("dbo.Vendors", "County", c => c.String());
-            AddColumn("dbo.Vendors", "Street", c => c.String());
-            AddColumn("dbo.Vendors", "HouseNo", c => c.String());
-            AddColumn("dbo.Vendors", "Name", c => c.String());
-            DropColumn("dbo.Vendors", "ZipCode");
-            DropColumn("dbo.Vendors", "Address2");
-            DropColumn("dbo.Vendors", "Address1");
-            DropColumn("dbo.Vendors", "CompanyName");
-            DropColumn("dbo.Vendors", "VendorId");
+        {          
             DropTable("dbo.ARInvoices");
             DropTable("dbo.ARChecks");
             DropTable("dbo.APInvoices");
--- AICloud.Accounts.Api/Migrations/202009291828381_BankAndLedger.cs
+++ AICloud.Accounts.Api/Migrations/202009291828381_BankAndLedger.cs
@@ -6,17 +6,7 @@
     public partial class BankAndLedger : DbMigration
     {
         public override void Up()
-        {
-            AddColumn("dbo.Vendors", "Name", c => c.String());
-            AddColumn("dbo.Vendors", "HouseNo", c => c.String());
-            AddColumn("dbo.Vendors", "Street", c => c.String());
-            AddColumn("dbo.Vendors", "County", c => c.String());
-            AddColumn("dbo.Vendors", "PinCode", c => c.String());
-            DropColumn("dbo.Vendors", "VendorId");
-            DropColumn("dbo.Vendors", "CompanyName");
-            DropColumn("dbo.Vendors", "Address1");
-            DropColumn("dbo.Vendors", "Address2");
-            DropColumn("dbo.Vendors", "ZipCode");
+        {          
             DropTable("dbo.APChecks");
             DropTable("dbo.APInvoices");
             DropTable("dbo.ARChecks");
@@ -93,18 +83,8 @@
                         ExchangeRate = c.Decimal(nullable: false, precision: 18, scale: 2),
                         Description = c.String(),
                     })
-                .PrimaryKey(t => t.Id);
-            
-            AddColumn("dbo.Vendors", "ZipCode", c => c.String());
-            AddColumn("dbo.Vendors", "Address2", c => c.String());
-            AddColumn("dbo.Vendors", "Address1", c => c.String());
-            AddColumn("dbo.Vendors", "CompanyName", c => c.String());
-            AddColumn("dbo.Vendors", "VendorId", c => c.String());
-            DropColumn("dbo.Vendors", "PinCode");
-            DropColumn("dbo.Vendors", "County");
-            DropColumn("dbo.Vendors", "Street");
-            DropColumn("dbo.Vendors", "HouseNo");
-            DropColumn("dbo.Vendors", "Name");
+                .PrimaryKey(t => t.Id);           
+        
         }
     }
 }
