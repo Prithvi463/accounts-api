19c5faccd9288a8dca33aa93934f55c230e477e6
Lakshman Dirisala <lakshman.dirisala@actlogica.com>
2020-10-01 00:14:59 +0530

    added json formatter for api calls

--- AICloud.Accounts.Api/App_Start/WebApiConfig.cs
+++ AICloud.Accounts.Api/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace AICloud.Accounts.Api
 {
@@ -19,6 +20,11 @@ namespace AICloud.Accounts.Api
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+			config.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+			config.Formatters.JsonFormatter.SupportedMediaTypes.Add(
+				new System.Net.Http.Headers.MediaTypeHeaderValue("application/json-patch+json"));
+
         }
     }
 }
